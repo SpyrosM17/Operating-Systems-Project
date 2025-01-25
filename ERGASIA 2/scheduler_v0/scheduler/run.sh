91ee0326fc9413a96dd98db5d85dc7fc5f56754c
Spyros <smantadake71@gmail.com>
2025-01-25 20:28:55 +0200

    sum

--- ERGASIA 2/scheduler_v0/scheduler/run.sh
+++ ERGASIA 2/scheduler_v0/scheduler/run.sh
@@ -3,8 +3,8 @@
 # Alexandros Georgios Chalampakis, 1100754
 # Panagiwths Papanikolaou, 1104804
 
-./scheduler_v1 FCFS homogeneous.txt 2   > fcfs_homogeneous.txt
+./scheduler_v2 FCFS homogeneous.txt 3   > fcfs_homogeneous.txt
 #./scheduler_v1 RR 1000 homogeneous.txt > rr1000_homogeneous.txt
 
-./scheduler_v1 FCFS reverse.txt 1 > fcfs_reverse.txt
+./scheduler_v2 FCFS reverse.txt 3  > fcfs_reverse.txt
 #./scheduler_v1 RR 1000 reverse.txt > rr1000_reverse.txt
